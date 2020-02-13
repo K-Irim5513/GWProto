ffdc2311bd5d4db4c6e8f320b39cad7c7ed33e46
K-Irim1415 <kitutuki277@gmail.com>
2020-02-13 18:32:28 +0900

    enemy追加

---
+++ New Unity Project (1)/Assets/Script/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public float speed = 3.0f;
+	// Start is called before the first frame update
+	void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position += transform.forward * speed * Time.deltaTime;
+		if (transform.position.z<0)
+		{
+			speed *= -1;
+		}
+		if (transform.position.z >5 )
+		{
+			speed *= -1;
+		}
+		
+	}
+}
